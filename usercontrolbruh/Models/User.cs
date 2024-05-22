8d9c173994bcadee693e984ce1eaacff6f0d3c52
Kovács Ferenc Richárd <kliberregobenedek@ktch.hu>
2024-05-22 09:08:30 +0200

    Add project files.

---
+++ usercontrolbruh/Models/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace usercontrolbruh.Models
+{
+    public class User : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value;
+                OnPropertyChanged();
+            }
+        }  
+
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public User()
+        {
+            Id = 0;
+        }
+
+        public User(string name, string email)
+        {
+            Id = 0;
+            Name = name;
+            Email = email;
+        }
+
+        public override string? ToString()
+        {
+            return $"{Name} ({Email})";
+        }
+    }
+}
