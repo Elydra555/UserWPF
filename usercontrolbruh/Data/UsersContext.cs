d9b8935c1c5fe151b2227bab1a4ae634af10ba59
Kovács Ferenc Richárd <kliberregobenedek@ktch.hu>
2024-05-22 09:46:47 +0200

    alapok

---
+++ usercontrolbruh/Data/UsersContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using usercontrolbruh.Models;
+
+namespace usercontrolbruh.Data
+{
+    public class UsersContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        public UsersContext() 
+        {
+        
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=SzervkereskedoBt;Trusted_Connection=True;");
+        }
+
+
+    }
+}
